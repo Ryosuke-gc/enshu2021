46741f988032ca777f99279b53ea22413c5d4f4e
西野祐希 <yuukinishino@d119.private>
2021-10-25 18:13:46 +0900

    ulr + re --> path

--- enshu_site/seat_app/urls.py
+++ enshu_site/seat_app/urls.py
@@ -1,13 +1,15 @@
 from django.conf.urls import url,include
 from django.contrib import admin
 from  seat_app.views import home
+from django.urls import path, include
+
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^index/', home.index),
-    url(r'^stuindex/', home.stuindex),
-    url(r'^login/', home.login),
-    url(r'^register/', home.register),
-    url(r'^userlist/', home.userlist),
+    path('admin/', admin.site.urls),
+    path('index/', home.index),
+    path('stuindex/', home.stuindex),
+    path('login/', home.login),
+    path('register/', home.register),
+    path('userlist/', home.userlist),
     # url(r'^adduser/', home.adduser),
     # url(r'^edituser/(\d+)/', home.edituser),
     # url(r'^deluser/(\d+)/', home.deluser),
